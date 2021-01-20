a61b2126fa743a9b1c05307cbe1ebe57cb6c9746
Obafemi Teminife <andysakov1958@gmail.com>
2021-01-20 01:45:49 +0100

    Made a new type to make the parameter type less ambigous

--- src/main/scala/com/quiza/api/types/Answer.scala
+++ src/main/scala/com/quiza/api/types/Answer.scala
@@ -4,10 +4,11 @@ class Answer {
 
 }
 object Answer {
+	type SingleAnswer = (String, Option[Double])
 	class DoubleAnswer(ans: Double) extends Answer {
 		def value: Double = ans.toFloat.toDouble
 	}
-	class QuadraticAnswer(vars: List[(String, Option[Double])]) extends Answer {
+	class QuadraticAnswer(vars: List[SingleAnswer]) extends Answer {
 		def value: List[(String, Option[Double])] = vars
 	}
 }
